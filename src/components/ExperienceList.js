75762ab36754a817be8c97c89690e97edf3e2036
19valentinavergara98 <valentina.98.3440@gmail.com>
2022-02-03 09:41:54 +0100

    solucionando problema en componente experiencelist

--- src/components/ExperienceList.js
+++ src/components/ExperienceList.js
@@ -3,7 +3,7 @@ import ExperienceForm from "./components/ExperienceForm";
 import Experience from "./components/Experience";
 
 
-function Experience() {
+function ExperienceList() {
     const [experiences, setExperiences] = useState([])
 
     const addExperiences = experience => {
@@ -11,7 +11,7 @@ function Experience() {
             return
         }
 
-        const experiences = [experience, ...experiences]
+        const experiences= [experience, ...experiences]
         setExperiences(newExperiences)
         console.log(newExperiences);
     };
