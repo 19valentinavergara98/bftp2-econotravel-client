af3811edf34b77c306cf5f3a2a9ee889a7447704
19valentinavergara98 <valentina.98.3440@gmail.com>
2022-02-07 10:29:51 +0100

    realizando diseño pagina pricipal

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -8,17 +8,22 @@ import { Button } from 'react-bootstrap';
 function Inicio(props){
     return(
         <div className="inicio-eco">
+            <div className= "nabvar">
             <ul>
                 <li>logo</li>
                 <li>QUIÉNES SOMOS</li>
                 <li>EXPERIENCIAS</li>
                 <li>CONTACTO</li>
             </ul>
-            <img src={tibidabo} className="imgTibidabo"></img>
-            <h1>¡ VIVE UNA EXPERIENCIA INOLVIDABLE !</h1>
-            <div className="actions">
-                <Button variant="success">DESCÚBRELAS AQUÍ</Button>
             </div>
+            <img src={tibidabo} className="imgTibidabo"></img>
+            <div className= "container">
+                <h1>¡ VIVE UNA EXPERIENCIA INOLVIDABLE !</h1>
+            </div>
+                <div className="button">
+                    <Button variant="success">DESCÚBRELAS AQUÍ</Button>
+                </div>
+
         </div>
     );
 }
