fff61420f5eb451b8b5514b76ad9d658ff1b4f88
19valentinavergara98 <valentina.98.3440@gmail.com>
2022-02-07 11:47:30 +0100

    añadido estilo a componente quienes somos

--- src/components/QuienesSomos.js
+++ src/components/QuienesSomos.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
+import "./QuienesSomos.css"
 
 function QuienesSomos(props){
     return(
         <div className="quienes-somos">
-            <p>ECONOTRAVEL promociona y diseña experiencias y actividades en Cataluña, fomentando un turismo responsable con la economía local, sostenible con el medio ambiente y ayudando a pequeños negocios de proximidad, fundaciones, asociaciones y proyectos locales a posicionarse en el sector turístico.</p>
-            <Button variant="success">Quiénes somos</Button>
+            <p>"ECONOTRAVEL promociona y diseña experiencias y actividades en Cataluña, fomentando un turismo responsable con la economía local, sostenible con el medio ambiente y ayudando a pequeños negocios de proximidad, fundaciones, asociaciones y proyectos locales a posicionarse en el sector turístico."</p>
+           <div className= "button-somos">
+                <Button variant="success" size="lg">Quiénes somos</Button>
+            </div>
         </div>
     );
 }
