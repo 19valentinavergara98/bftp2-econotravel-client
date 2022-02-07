9268c23d207b4da0e8ccfd8093a103e37372266b
19valentinavergara98 <valentina.98.3440@gmail.com>
2022-02-07 09:37:08 +0100

    haciendo merge para realizar pull de los cambios de laura

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from "./Navbar";
 import './Inicio.css';
 import tibidabo from "./assets/tibidabo.jpeg"
 
@@ -7,8 +6,13 @@ import tibidabo from "./assets/tibidabo.jpeg"
 function Inicio(props){
     return(
         <div className="inicio-eco">
-            <Navbar />
-            <img src={tibidabo}></img>
+            <ul>
+                <li>logo</li>
+                <li>QUIÉNES SOMOS</li>
+                <li>EXPERIENCIAS</li>
+                <li>CONTACTO</li>
+            </ul>
+            <img src={tibidabo} className="imgTibidabo"></img>
             <h1>¡ VIVE UNA EXPERIENCIA INOLVIDABLE !</h1>
             <div className="actions">
                 <button>DESCÚBRELAS AQUÍ</button>
