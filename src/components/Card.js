77c0375d9d9487a1412605f8e4785326fa371c36
19valentinavergara98 <valentina.98.3440@gmail.com>
2022-02-11 01:34:42 +0100

    ningun cambio

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,41 +11,49 @@ function Card(props) {
 
     return (
         <div className="card-container">
-
             <div className="card">
 
-                <h2 className="experience-name">
-                    {props.experience.name}
-                </h2>
-                <p className="card-description">
-                    {props.experience.description}
-                </p>
-                <p className="card-price">
-                    {props.experience.price}€
-                </p>
-                <p className="card-duration">
-                    {props.experience.duration}
-                </p>
-                <p className="card-tag">
-                    {props.experience.tag}
-                </p>
-                <div className="img-container">
-                    {props.experience.coverImage}
-                    <img src={tibidabo} className="tibidabo"></img>
+                <div className="container2">
+                    <div className="card1">
+                        <div className="border">
+                            <div className="card-img">
+                                {props.experience.coverImage}
+                                <img src={tibidabo} className="tibidabo"></img>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="action-buttons">
-                    <Button variant="success">Saber más</Button>
+                <div className="parametros">
+                    <h2 className="experience-name">
+                        {props.experience.name}
+                    </h2>
+                    <p className="card-description">
+                        {props.experience.description}
+                    </p>
+                    <p className="card-price">
+                        {props.experience.price}€
+                    </p>
+                    <p className="card-duration">
+                        {props.experience.duration}
+                    </p>
+                    <p className="card-tag">
+                        {props.experience.tag}
+                    </p>
+                </div>
+                    <div className="action-buttons">
+                        <Button variant="success">Saber más</Button>
 
-                </div>
-                <div className="admin-buttons">
-                    <Button variant="secondary">Edit</Button>
-                    <Button variant="danger">Delete</Button>
+                    </div>
+                    <div className="admin-buttons">
+                        <Button variant="secondary">Edit</Button>
+                        <Button variant="danger">Delete</Button>
 
-                </div>
+                    </div>
 
             </div>
         </div>
-    );
+
+    )
 }
 
 export default Card;
