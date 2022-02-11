bf46092bd580342c337b0419430b3aa6f8a11dfb
SMoliner <komissura@gmail.com>
2022-02-11 11:02:22 +0100

    estilo tarjetas

--- src/components/Card.js
+++ src/components/Card.js
@@ -15,15 +15,16 @@ function Card(props) {
 
                 <div className="container2">
                     <div className="card1">
-                        <div className="border">
+                        <figure class name="card-thumb">
+
                             <div className="card-img">
                                 {props.experience.coverImage}
-                                <img src={tibidabo} className="tibidabo"></img>
+                                <img src={tibidabo} className="tibidabo"alt="Picture by Kyle Cottrell"></img>
                                 <h2 className="experience-name">
                                     {props.experience.name}
                                 </h2>
                             </div>
-                        </div>
+                        </figure>
                     </div>
                 </div>
                 <div className="parametros">
