7cb66285f8eec00c9b160b275eba5fe994251821
Laura Contreras <m.laura.cp89@gmail.com>
2022-02-21 12:42:00 +0100

    cards retocadas

--- src/components/Experiencias.js
+++ src/components/Experiencias.js
@@ -5,9 +5,9 @@ import "./Experiencias.css"
 function Experiencias(props) {
     return (<div className="experiences" id="experiencias">
                 <div className="parrafo">
-                    <p>Nuestras experiencias han sido cuidadosamente seleccionadas para que puedas contribuir al desarrollo de
+                    Nuestras experiencias han sido cuidadosamente seleccionadas para que puedas contribuir al desarrollo de
                     la economía local y a la conservación del medio ambiente, siguiendo las pautas establecidas en el marco
-                    del turismo responsable y sostenible....</p>
+                    del turismo responsable y sostenible....
                 </div>
             <div className="experiences-body">
                 {props.experiencias.map(experience => <Card experience={experience}/>)}
